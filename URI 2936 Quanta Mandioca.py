68976662f4a99247307be081077e8cb6b55c2e05
Roberto Martins <robertomartins@id.uff.br>
2022-07-02 12:49:00 -0300

    Muda nome de classe para seguir convençao de nomes

--- URI 2936 Quanta Mandioca.py
+++ URI 2936 Quanta Mandioca.py
@@ -1,10 +1,10 @@
-class porcao:
+class Porcao:
     def __init__(self, qtd, tamanho):
         self.qtd = qtd
         self.tamanho = tamanho
 
 total_mandioca = 0
-lista_porcoes = [porcao(int(input()), 300), porcao(int(input()), 1500), porcao(int(input()), 600), porcao(int(input()), 1000), porcao(int(input()), 150), porcao(1, 225)]
+lista_porcoes = [Porcao(int(input()), 300), Porcao(int(input()), 1500), Porcao(int(input()), 600), Porcao(int(input()), 1000), Porcao(int(input()), 150), Porcao(1, 225)]
 for objeto in lista_porcoes:
     total_mandioca += objeto.qtd * objeto.tamanho
 print(total_mandioca)
